50c677eaf77c1f851392f1c2bff02604e30c5e22
harshit <harshitgupta1406@gmail.com>
2023-11-16 16:06:02 +0530

    [backend] Add Place model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Place
 # Register your models here.
+
+@admin.register(Place)
+class PlaceAdmin(OSMGeoAdmin):
+    list_display = ('name', 'location')
--- places/models.py
+++ places/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.gis.db import models as gis_models
+import uuid
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class PlaceTypeChoices(models.TextChoices):
+    RESTAURANT = 'R', _('Restaurant')
+    TOURIST_SPOT = 'T', _('Tourist Spot')
+    SHOP = 'S', _('Shop')
+    OTHERS = 'O', _('Others')
+
+class Place(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    location = gis_models.PointField()
+    description = models.TextField(max_length=1000, blank=True)
+    type = models.CharField(max_length=1, choices=PlaceTypeChoices.choices, default=PlaceTypeChoices.OTHERS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
