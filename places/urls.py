0022c74fe147f2be75cdd979c9e7dee3088d13cd
harshit <harshitgupta1406@gmail.com>
2023-11-17 12:29:55 +0530

    [Places] Add endpoint to calculate the shortest path between two points

--- places/urls.py
+++ places/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PlaceList
+from .views import PlaceList, get_route
 
 urlpatterns = [
     path('',PlaceList.as_view(), name="places_list"),
+    path('/route',get_route, name="get_route")
 ]
--- places/views.py
+++ places/views.py
@@ -1,8 +1,11 @@
 from rest_framework.generics import ListAPIView
+from django.http import JsonResponse
 from rest_framework_gis.filters import DistanceToPointFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Place
 from .serializers import PlaceSerializer
+import osmnx as ox
+import networkx as nx
 
 class PlaceList(ListAPIView):
     queryset = Place.objects.all()
@@ -24,3 +27,20 @@ class PlaceList(ListAPIView):
         response = super().get(request, *args, **kwargs)
 
         return response
+
+def get_route(request):
+    location = ["Palo Alto, California", "Mountain View, California"]
+    start = request.GET.get('start').split(',')
+    end = request.GET.get('end').split(',')
+    G = ox.graph_from_place(location, network_type="drive")
+    G = ox.add_edge_speeds(G)
+    G = ox.add_edge_travel_times(G)
+ 
+    start_node = ox.distance.nearest_nodes(G, float(start[1]), float(start[0]))
+    end_node = ox.distance.nearest_nodes(G, float(end[1]), float(end[0]))
+    route = nx.shortest_path(G, start_node, end_node, weight="length")
+
+    route_geometry = ox.utils_graph.route_to_gdf(G, route,"length")
+    distance = int(sum(route_geometry["length"]))
+    time = int(sum(ox.utils_graph.route_to_gdf(G, route, "travel_time")["travel_time"]))
+    return JsonResponse({'route_geojson': route_geometry.to_json(), 'distance':distance, 'time':time})
